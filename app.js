16bce5a6578704bb2515ca35a47750557cddf2a1
Samyak <71600450+samyak2003-coder@users.noreply.github.com>
2023-08-22 18:32:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express= require('express')
+const bodyParser=require('body-parser')
+const app= express()
+
+const date=require(__dirname+'/date.js')
+
+let items=  ['Buy Food','Cook Food','Eat Food']
+let workItems=[]
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+app.get('/', function(req,res){
+
+  let day=date.getDate()
+res.render('list',{listTitle:day, newListItem:items})
+})
+
+app.post('/',function(req,res){
+let item=req.body.newItem
+
+if (req.body.list==='Work'){
+  workItems.push(workItems)
+  res.redirect('/work')
+} else {
+items.push(item)
+res.redirect('/')
+}
+
+})
+
+app.get('/work',function(req,res){
+
+  res.render('list',{listTitle:'Work List',newListItem:workItems})
+})
+
+app.get('/about',function(req,res){
+  res.render('about')
+})
+
+app.listen(3000,function(){
+  console.log('Server is running on port 3000')
+})
